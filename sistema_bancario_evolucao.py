9c05761f2f3287f1cacd52eb879ec817fbf74a12
luanaslv <luana.slv@ilcoud.com>
2023-08-12 14:53:17 -0300

    correção saque e depósito zero

--- sistema_bancario_evolucao.py
+++ sistema_bancario_evolucao.py
@@ -5,40 +5,41 @@ def funcao_verifica_valor():
         try:
             valor = float(input("\nInsira o valor que deseja: "))
             if valor < 0:
-               saida_texto("\nO valor precisa ser maior que zero.")
+               saida_texto("O valor precisa ser maior que zero. Digite zero para retornar ao MENU.")
             else:
-                valor_formatado = f"R$ {valor:.2f}"
-                return valor,valor_formatado
+                return valor
         except ValueError:
-            saida_texto("\nValor inválido! Use apenas números para os valores e separe o real do centavo com ponto final (.)")    
+            saida_texto("Valor inválido! Use apenas números para os valores e separe o real do centavo com ponto final (.)")    
 
-def funcao_saque(valor, extrato, data_formatada, saldo, numero_de_saques, limite_de_saques, limite, valor_formatado):
+def funcao_verifica_saque(valor, saldo, numero_de_saques, limite_de_saques, limite):
     if valor > limite:
-        saida_texto("Limite máximo por saque é de R$ 500.00\n")
+        saida_texto("Limite máximo por saque é de R$ 500.00")
     elif valor > saldo:
-        saida_texto(f"Você não possue saldo suficiente. Seu saldo atual é de {saldo:.2f}.\n")
+        saida_texto(f"Você não possue saldo suficiente. Seu saldo atual é de {saldo:.2f}.")
     elif numero_de_saques == limite_de_saques:
         saida_texto("Você alcançou seu limite de saque diário. Retornar amanhã.")
     else:
-        extrato = extrato + "Saque:    " + data_formatada.ljust(25, '-') + valor_formatado + "\n"
-        data_ultimo_saque = data_formatada
-        saldo -= valor
-        numero_de_saques += 1
-        saida_texto("Operação realizada com sucesso!")
-        return numero_de_saques, extrato, data_ultimo_saque, saldo   
+        return True
 
-def funcao_depositar(valor,extrato,saldo,data_formatada,valor_formatado):
-    extrato = extrato + "Depósito: " + data_formatada.ljust(25, '-') + valor_formatado + "\n"
+def funcao_saque(valor, extrato, data_formatada, saldo, numero_de_saques):
+    extrato = extrato + "Saque:    " + data_formatada.ljust(25, '-') + f"R$ {valor:.2f}" + "\n"
+    data_ultimo_saque = data_formatada
+    saldo -= valor
+    numero_de_saques += 1
+    saida_texto("Operação realizada com sucesso!")
+    return numero_de_saques, extrato, data_ultimo_saque, saldo   
+
+def funcao_depositar(valor, extrato, saldo, data_formatada):
+    extrato = extrato + "Depósito: " + data_formatada.ljust(25, '-') + f"R$ {valor:.2f}" + "\n"
     saldo += valor
     saida_texto("Operação realizada com sucesso!")
-    return saldo,extrato
+    return saldo, extrato
             
 def saida_texto(texto):
     print(texto)
 
 menu = '''
 Olá! Estamos acessando sua conta bancária, digite a opção que deseja, sendo:
-
 [1] - Depositar
 [2] - Sacar
 [3] - Extrato
@@ -47,11 +48,10 @@ Olá! Estamos acessando sua conta bancária, digite a opção que deseja, sendo:
 '''
 saldo = 0
 limite = 500
-extrato = ""
+extrato = "EXTRATO BANCÁRIO:\n"
 numero_de_saques = 0
 limite_de_saques = 3
 data_ultimo_saque = ""
-valor_formatado = ""
 
 while True:
     data_atual = datetime.now().date()
@@ -60,15 +60,18 @@ while True:
 
     if opcao == 1:
         saida_texto("\nBem vindo(a) a sua área de depósito.\nSegue modelo de como deve ser inserido o valor a ser depositado: 45.67\nUse ponto final (.) para separar a casa decimal.")
-        valor,valor_formatado = (funcao_verifica_valor())
-        saldo,extrato = funcao_depositar(valor,extrato,saldo,data_formatada,valor_formatado)  
+        valor = funcao_verifica_valor()
+        if valor != 0:
+            saldo, extrato = funcao_depositar(valor, extrato, saldo, data_formatada)  
        
     elif opcao == 2:
         saida_texto("\nBem vindo(a) a sua área de saque.\nSegue modelo de como deve ser inserido o valor a ser retirado: 45.67\nUse ponto final (.) para separar a casa decimal.")
-        valor, valor_formatado = funcao_verifica_valor()
+        valor = funcao_verifica_valor()
         if data_ultimo_saque != data_formatada:
             numero_de_saques = 0
-        numero_de_saques, extrato, data_ultimo_saque, saldo = funcao_saque(valor = valor, extrato = extrato, data_formatada = data_formatada, saldo = saldo, numero_de_saques = numero_de_saques, limite_de_saques = limite_de_saques, limite = limite, valor_formatado = valor_formatado)
+        status = funcao_verifica_saque(valor, saldo, numero_de_saques, limite_de_saques, limite)
+        if status == True and valor != 0:
+            numero_de_saques, extrato, data_ultimo_saque, saldo = funcao_saque(valor, extrato, data_formatada, saldo, numero_de_saques)
     
     elif opcao == 3:
         print("Não foram realizadas movimentações." if not extrato else extrato)
